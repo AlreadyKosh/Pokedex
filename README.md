178616b67d48fe79a5f5917e550dd87eef3554ed
Douglas Carreira <lenusui123@gmail.com>
2023-12-05 22:31:30 -0300

    HTML info inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">Pokedex - PokeAPI</h1>
+<p align="center">Landing page simulating a Pokédex from the Pokemon anime, which is a virtual encyclopedia that contains all Pokémon species and their appropriate information.</p>
+
+<h3 align="center">Link: https://alreadykosh.github.io/Pokedex/</h3>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
+const loadDetailPokemon = document.querySelector(".pokemons");
 const maxRecords = 151;
 const limit = 5;
 let offset = 0;
@@ -9,21 +10,27 @@ function loadPokemonItens(offset, limit) {
         const newHtml = pokemons
             .map(
                 (pokemon) => `
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-                    <ol class="types">
-                    ${pokemon.types
-                        .map((type) => `<li class="type ${type}">${type}</li>`)
-                        .join("")}
-                    </ol>
-                    <img
-                        src="${pokemon.photo}"
-                        alt="${pokemon.name}"
-                    />
-                </div>
-            </li>`
+            <a  href="#" onclick="${pokemon.number}">
+                <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                        <ol class="types">
+                        ${pokemon.types
+                            .map(
+                                (type) =>
+                                    `<li class="type ${type}">${type}</li>`
+                            )
+                            .join("")}
+                        </ol>
+                        <img
+                            src="${pokemon.photo}"
+                            alt="${pokemon.name}"
+                        />
+                    </div>
+                </li>
+            </a>
+            `
             )
             .join("");
 
@@ -36,7 +43,6 @@ loadPokemonItens(offset, limit);
 loadMoreButton.addEventListener("click", () => {
     offset += limit;
     const qtdRecordsWithNexPage = offset + limit;
-
     if (qtdRecordsWithNexPage >= maxRecords) {
         const newLimit = maxRecords - offset;
         loadPokemonItens(offset, newLimit);
@@ -45,4 +51,9 @@ loadMoreButton.addEventListener("click", () => {
     } else {
         loadPokemonItens(offset, limit);
     }
+});
+
+loadDetailPokemon.addEventListener("click", () => {
+    loadDetailPokemon.classList.add("display-none");
+    
 });
